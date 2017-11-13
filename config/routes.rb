12b3abad29b91e24f5ec1a61228b0d2b7241793c
ValentinB <valentin.bredy@hotmail.fr>
2017-11-13 13:16:27 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  root :to => 'posts#index'	
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
